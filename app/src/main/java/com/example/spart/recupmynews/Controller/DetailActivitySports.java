e317468e16903ac9964d8ace4bfb245bb55a444c
spartam72 <spartam72@yahoo.fr>
2019-05-03 12:42:05 +0200

    vendredi commit 2

---
+++ app/src/main/java/com/example/spart/recupmynews/Controller/DetailActivitySports.java
@@ -0,0 +1,38 @@
+package com.example.spart.recupmynews.Controller;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.webkit.WebView;
+
+import com.example.spart.recupmynews.MySportsAPI.ArticleSport;
+import com.example.spart.recupmynews.MyTopStoriesAPI.ArticleTopStories;
+import com.example.spart.recupmynews.R;
+
+import java.util.Objects;
+
+
+public class DetailActivitySports extends AppCompatActivity {
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView( R.layout.activity_detail_sports);
+
+        Toolbar toolbar =  findViewById( R.id.toolbar );
+        setSupportActionBar( toolbar );
+        Objects.requireNonNull( getSupportActionBar() ).setTitle( "Details from Sports Article" );
+        toolbar.setBackgroundColor( getResources().getColor( R.color.colorSport ) );
+        getSupportActionBar().setDisplayHomeAsUpEnabled( true );
+
+        WebView webViewDetail = findViewById( R.id.webViewDescription );
+        ArticleSport result = (ArticleSport) Objects.requireNonNull( getIntent().getExtras() ).getSerializable( "result" );
+        webViewDetail.loadUrl( Objects.requireNonNull( result ).getUrl());
+
+
+
+    }
+    
+}
